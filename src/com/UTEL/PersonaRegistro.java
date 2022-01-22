1496839ab7c5c1ff4087c1778ebb3367cad0ff3a
lunao <lunaortiz1244@gmail.com>
2022-01-22 18:27:42 -0500

    se reemplaza clase persona por registro persona

--- src/com/UTEL/PersonaRegistro.java
+++
@@ -1,9 +0,0 @@
-package com.UTEL;
-
-public record PersonaRegistro(String nombre, String apellidos) {
-    @Override
-    public String toString() {
-        return "%s %s" .formatted(nombre, apellidos);
-
-    }
-}
