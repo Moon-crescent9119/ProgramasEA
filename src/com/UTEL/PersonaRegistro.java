3f14548b0ab23e50444d11f4e9396f9b909fb060
lunao <lunaortiz1244@gmail.com>
2022-01-22 18:19:27 -0500

    se quitó la línea de código insevible.

--- src/com/UTEL/PersonaRegistro.java
+++ src/com/UTEL/PersonaRegistro.java
@@ -3,7 +3,6 @@ package com.UTEL;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        System.out.println("Está instrucción es inservible");
         return "%s %s" .formatted(nombre, apellidos);
 
     }
